f7c97733412b05e17c7f18157c1bde59f05268ca
lo <604647004@qq.com>
2024-10-12 10:02:10 +0800

    .

---
+++ springboot-open-office/src/main/java/org/example/springbootopenoffice/SpringbootOpenOfficeApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootopenoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOpenOfficeApplication.class, args);
+    }
+
+}
